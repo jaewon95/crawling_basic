c9cd6086dc06bd8fcf31740fc8a3631f1a7a0487
jw <ckvk2010@naver.com>
2023-12-01 01:22:51 +0900

    쿠팡 크롤링 기초 연습. 강의 보기전 미리 해보기.
    광고 제외, 상품순위, 상품명, 상품 가격
    
    배열에 code (검색어), 순위, 상품명, 가격 담아보기 성공.
    
    쿠팡은 cookie 값 필수, 벤 당하지 않으려면 어떻게 해야하는지는 더 찾아봐야함. 일단 timeout 으로 기본 설정해둔 상태임.
    
    강의 듣고 또 복습하면서 만들기

---
+++ 04.coupang_self.py
@@ -0,0 +1,60 @@
+import requests
+import random
+from bs4 import BeautifulSoup
+
+b_url = "https://www.coupang.com/np/search?component=&q="
+
+# 임의로 생성한 랜덤값. 차단 막기위함.
+random_numbers = random.sample(range(101, 131), 3)
+random_data = ''.join(map(str, random_numbers))
+
+headerNum = int(random_data) + 21
+
+
+headers = {
+    "User-Agent" : f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
+cookie = {'asd125as' : str(headerNum) }
+
+keyword = input("검색어를 입력하세요 : ")
+
+search_url = b_url + keyword
+
+#쿠팡은 cookie를 요구한다
+
+
+
+# timeout : 지연시간 설정
+req = requests.get(search_url,timeout=5, headers=headers, cookies=cookie)
+
+site = req.text
+
+soup = BeautifulSoup(site, "html.parser")
+
+findNumber = soup.select('.search-product-link:not(.ad-badge)') # .ad-badge 가 있는건 빼고 가져오고 싶다. :not()
+rankingProduct = []
+number = 0
+for i in findNumber :
+    if i.find(class_='number') != None :
+        print(' >>>> 순위상품 <<<<')
+        print('순위 : ',i.find(class_='number').text)
+        print('상품명 : ',i.find(class_='name').text.strip())
+        print('가격 : ', i.find(class_='price-area').find(class_='price-value').text) # 자식요소 찾기
+        
+        rankingProduct.append({
+            'code' : keyword,
+            'number' : i.find(class_='number').text,
+            'item' : {
+                '순위' : i.find(class_='number').text,
+                '상품명' : i.find(class_='name').text.strip(),
+                '가격' : i.find(class_='price-area').find(class_='price-value').text
+            }
+        })    
+    else :
+        print('순위 : ',i.find(class_='number'))    
+        print('상품명 : ',i.find(class_='name').text.strip())
+        print('가격 : ', i.find(class_='price-area').find(class_='price-value').text) # 자식요소 찾기
+        print('')
+#
+    
+print(rankingProduct)
