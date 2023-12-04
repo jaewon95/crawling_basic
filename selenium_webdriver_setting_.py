d502751fd1b7dc88cdba28f60c43929ea18c873d
jw <winangil153@gmail.com>
2023-12-04 21:38:15 +0900

    셀레니움 4.6 이상 버전부터는 간단하게 웹 드라이버 자동화가 가능하다

--- selenium_webdriver_setting_.py
+++ selenium_webdriver_setting_.py
@@ -1,11 +1,21 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service #webdriver-manager set
+# from webdriver_manager.chrome import ChromeDriverManager #webdriver-manager set
+# import time
+
+# service = Service(ChromeDriverManager().install())
+
+# driver = webdriver.Chrome(service=service)
+
+# driver.get("https://naver.com")
+# time.sleep(3)
+
+
+
+# 4.6 이후부터는 간단하게 웹 드라이버 자동화 가능
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service #webdriver-manager set
-from webdriver_manager.chrome import ChromeDriverManager #webdriver-manager set
 import time
 
-service = Service(ChromeDriverManager().install())
-
-driver = webdriver.Chrome(service=service)
-
+driver = webdriver.Chrome()
 driver.get("https://naver.com")
 time.sleep(3)
